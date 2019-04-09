f3a12beb0df29136ab4139e9cd913fd6423489dc
Filipe Paiva <filipepaiva240@gmail.com>
2019-04-09 19:18:11 -0300

    Initial commit: Add header and  app components. Add Css stylesheet

---
+++ google-homepage/src/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<header className="header">
+			<nav className="nav-bar">
+				<ul className="menu">
+					<li className="menu-item">Gmail</li>
+					<li className="menu-item"> Imagens</li>
+					<li className="menu-item">Opções</li>
+					<li className="menu-item">Conta</li>
+				</ul>
+			</nav>
+		</header>
+	);
+};
+
+export default Header;
